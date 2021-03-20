18cd8b5a845bf78b1d59dddbd5fe55ce756a49e6
Vanica <roseatovar@gmail.com>
2021-03-20 13:30:41 -0500

    Initial Commit Again

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private float mainThrust = 1000f;
+    [SerializeField] private float rotationThrust = 100f;
+    private Rigidbody _rigidbody;
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            var scaledThrust = Vector3.up * (mainThrust * Time.deltaTime);
+            _rigidbody.AddRelativeForce(scaledThrust);
+        }
+        
+    }
+
+    private void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationThrust);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotationThrust);
+        }
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        //freezing rotation to manually rotate
+        _rigidbody.freezeRotation = true;
+        transform.Rotate(Vector3.forward * (Time.deltaTime * rotationThisFrame));
+        _rigidbody.freezeRotation = false;
+    }
+}
